ab6c3fdad7c6b5a6a22663c86493f53fad12da4a
inspiro-sk <development@inspiro.sk>
2022-08-21 14:04:25 +0200

    env variables added

--- simple_flask_webapp/app.py
+++ simple_flask_webapp/app.py
@@ -1,10 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+bg_color= os.environ.get('APP_COLOR')
 
 @app.route("/")
 def main():
-    return "Welcome"
+    print(bg_color)
+    return render_template('index.html', bg_color=bg_color)
 
 
 @app.route('/how_are_you')
